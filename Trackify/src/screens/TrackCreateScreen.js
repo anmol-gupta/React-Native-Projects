e8b95fe80caec187e793899ff456867fba8db6d7
Anmol Gupta <anmolify@gmail.com>
2020-04-10 23:51:02 +0530

    User fake location logic

--- Trackify/src/screens/TrackCreateScreen.js
+++ Trackify/src/screens/TrackCreateScreen.js
@@ -1,14 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
+import "../_mockLocation";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-navigation";
 import Map from "../components/Map";
-import { requestPermissionsAsync } from "expo-location";
+import {
+  requestPermissionsAsync,
+  watchPositionAsync,
+  Accuracy
+} from "expo-location";
 const TrackCreateScreen = () => {
   const [err, setErr] = useState(null);
   const startWatching = async () => {
     try {
       await requestPermissionsAsync();
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10
+        },
+        location => {
+          console.log(location);
+        }
+      );
     } catch (e) {
       console.log(e);
       setErr(e);
